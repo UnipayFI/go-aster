f85bddd668ad9b3aae6945d751a8df8a452d47ec
guanqi-lab <q42722180@gmail.com>
2025-12-16 19:34:37 +0800

    fix get commissionRate

--- spot/market_data.go
+++ spot/market_data.go
@@ -394,7 +394,7 @@ func (c *SpotClient) NewCommissionRateService(symbol string) *CommissionRateServ
 }
 
 func (s *CommissionRateService) Do(ctx context.Context) (*CommissionRateResponse, error) {
-	req := request.Get(ctx, s.c, "/api/v1/commissionRate", s.params)
+	req := request.Get(ctx, s.c, "/api/v1/commissionRate", s.params).WithSignature()
 	return request.Do[CommissionRateResponse](req)
 }
 
